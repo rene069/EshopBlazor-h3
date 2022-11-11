96caa1aaf8efd761d79bbecefbec9d2bd2e49146
Rene Rasmussen <rene069g@elevcampus.dk>
2022-11-11 09:25:40 +0100

    done

--- EshopBlazorProject---H3/Models/ProduktDTO.cs
+++ EshopBlazorProject---H3/Models/ProduktDTO.cs
@@ -9,16 +9,16 @@ namespace EshopBlazor.Models
         [Required,MaxLength(25)]
         public string ProduktName { get; set; }
         [Required, MaxLength(100)]
-        public string Description { get; set; } = "bla bla bla description";
+        public string Description { get; set; }
         [Required]
         public decimal Price { get; set; }
+        [Required]
         public int BrandId { get; set; }
+        [Required]
         public int TypesId { get; set; }
         public string? ImageURL { get; set; }
         public bool IsSoftDeleted { get; set; } = false;
 
-        public BrandDTO? Brand { get; set; }
-        public TypesDTO? Type { get; set; }
 
     }
 }
