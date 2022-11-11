292bb88d62ba92d3bab8defa7b399cc38d122f87
Rene Rasmussen <rene069g@elevcampus.dk>
2022-11-11 09:50:21 +0100

    added summary

--- EshopBlazorProject---H3/Models/ImageFile.cs
+++ EshopBlazorProject---H3/Models/ImageFile.cs
@@ -1,5 +1,9 @@
 ﻿namespace EshopBlazor.Models
 {
+
+    /// <summary>
+    /// NOT GETTING USED
+    /// </summary>
     public class ImageFile
     {
         public string base64data { get; set; }
